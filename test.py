915de00c92c7ed9ef77091705da1d91f82563859
Daniyal Bokhari <63890998+daniyalb@users.noreply.github.com>
2022-08-13 01:38:20 -0400

    Improved gender only and furnished/pets finding
    Improved the ability for the program to read the title and description to see if the listing says that they are looking for only male/female students.
    
    Fixed issue with finding if listing says accommodation is furnished as well as finding out if its pet friendly.

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
+from operator import is_
 from bs4 import BeautifulSoup
 import requests
 
+t1f = 'female'
+t2f = 'girl'
+t1sf = 'females'
+t2sf = 'girls'
+t1m = ' male'
+t2m = 'boy'
+t1sm = ' males'
+t2sm = 'boys'
+t3 = ' only'
+t4 = ' student'
+COMBOS_F = (t1f + t3, t2f + t3, t3 + t1f, t3 + t2f, t1sf + t3, t2sf + t3, t3 + t1sf,
+             t3 + t2sf, t1f + t4, t1f, t2f)
+COMBOS_M = (t1m + t3, t2m + t3, t3 + t1m, t3 + t2m, t1sm + t3, t2sm + t3, t3 + t1sm,
+             t3 + t2sm, t1m + t4, t1m, t2m)             
+
 #city = input('Which city would you like to search in? ')
 #city = city.lower() TODO: Unhide this and delete next line
-city = 'mississauga / peel region'
+city = 'toronto'
 max_price = input('What is the highest price you are willing to rent for? $')
 max_price = int(max_price)
 pets = ''
@@ -15,11 +31,9 @@ while not furnished == 'y' and not furnished == 'n':
 female_only = ''
 while not female_only == 'y' and not female_only == 'n':
     female_only = input('Would female only accomodations work for you? (y/n): ')
-"""
 shared = ''
 while not shared == 'y' and not shared == 'n':
     shared = input('Would you be okay with shared accomodations? (y/n): ')
-"""
 print('')
 
 def url_city_adder(city: str) -> str:
@@ -42,7 +56,7 @@ def url_city_adder(city: str) -> str:
     elif city == 'kingston':
         return 'https://www.kijiji.ca/b-for-rent/kingston-on/student/k0c30349001l1700183?rb=true&ad=offering'
     elif city == 'london':
-        return 'https://www.kijiji.ca/b-for-rent/london/student/k0c30349001l1700214?rb=true&ad=offering'
+        return 'https://www.kijiji.ca/b-for-rent/london/student/k0c30349001l1700214?rb=true&ad=offering' # TODO: London causes error
 
 def price_formatter(price: str) -> str:
     price = price[1:]
@@ -50,41 +64,46 @@ def price_formatter(price: str) -> str:
     price = price.replace(',', '')
     return int(price)
 
+def _check_included(info) -> bool:
+    included = info.li.dl.dd.text
+    if included == 'Yes':
+        return True
+    return False
+
 def link_explorer(link: str) -> tuple:
     html_text = requests.get(link).text
     content = BeautifulSoup(html_text, 'lxml')
-    furnished = content.find('dl', class_='itemAttribute-3080139557')
-    if furnished is None:
-        is_furnished = 'unknown'
-    elif furnished.dd.text == 'Yes':
-        is_furnished = True
-    else:
-        is_furnished = False
-    pets = content.find('dl', class_='itemAttribute-3080139557')
-    if pets is None:
-        pets_allowed = 'unknown'
-    elif pets.dd.text == 'Yes':
-        pets_allowed = True
-    else:
-        pets_allowed = False
+
+    holder = content.find_all('ul', class_='itemAttributeList-1090551278') 
+    # TODO: These listings have different setup: 
+    # https://www.kijiji.ca/v-apartments-condos/city-of-toronto/all-inclusive-5-bedroom-2-bathroom-house/1628835421
+    # https://www.kijiji.ca/v-apartments-condos/city-of-toronto/one-bedroom-basement-apartment-for-rent/1628843352
+    is_furnished = pets_allowed = 'unknown'
+    for info in holder:
+        if not info.li is None:
+            name = info.li.dl.dt.text
+            if name == 'Furnished':
+                is_furnished = _check_included(info)
+            else:
+                pets_allowed = _check_included(info)
+
     location = content.find('span', class_='address-3617944557').text
     description = content.find('div', class_='descriptionContainer-231909819').div.p.text
+
     return (is_furnished, pets_allowed, location, description)
 
-def check_female_only(title: str, description: str) -> bool:
+def check_gender_only(gender: str, title: str, description: str) -> bool:
     """ Checks if title or description contain any wording
-    if the listing is for females only, then sees if user
-    would be okay with female only accomodations, returns
+    if the listing is for males or females only, then sees if user
+    would be okay with those accomodations, returns
     True if compatible, returns False otherwise.
     """
-    t1 = 'female'
-    t2 = 'girl'
-    t1s = 'females'
-    t2s = 'girls'
-    t3 = 'only'
     title = title.lower()
     description = description.lower()
-    combos = (t1 + ' ' + t3, t2 + ' ' + t3, t3 + ' ' + t1, t3 + ' ' + t2, t1s + ' ' + t3, t2s + ' ' + t3, t3 + ' ' + t1s, t3 + ' ' + t2s)
+    if gender == 'female':
+        combos = COMBOS_F
+    else:
+        combos = COMBOS_M
     for combo in combos:
         if combo in title or combo in description:
             return True
@@ -112,18 +131,19 @@ for ad in ads:
         continue
     title_link = ad.find('div', class_='title').a
     link = 'https://www.kijiji.ca' + title_link['href']
-    is_furnished, pets_allowed, location, description = link_explorer(link)
+    is_furnished, pets_allowed, location, description = link_explorer(link) # TODO: FIX PETS ALLOWED
     if pets == 'y' and (not pets_allowed or pets_allowed == 'unknown'):
         continue
     if furnished == 'y' and (not is_furnished or is_furnished == 'unknown'):
         continue
     title = title_link.text.strip()
-    only_females = check_female_only(title, description)
-    listings[number] = (price, time, link, is_furnished, pets_allowed, location, description, title, only_females)
+    only_females = check_gender_only('female', title, description)
+    only_males = check_gender_only('male', title, description)
+    listings[number] = (price, time, link, is_furnished, pets_allowed, location, description, title, only_females, only_males)
     number += 1
 
 for listing in listings:
-    price, time, link, is_furnished, pets_allowed, location, description, title, only_females = listings[listing]
+    price, time, link, is_furnished, pets_allowed, location, description, title, only_females, only_males = listings[listing]
     print(title)
     print(f'Price: {price}')
     print(f'Posted: {time}')
@@ -132,4 +152,5 @@ for listing in listings:
     print(f'Pets allowed: {pets_allowed}')
     print(location)
     print(f'ONLY FEMALES: {only_females}')
+    print(f'ONLY MALES: {only_males}')
     print('----------------------------------------------------------------')
